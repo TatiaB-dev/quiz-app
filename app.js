adb9b0cfe407e7d756e0a4e86779b27932697edf
Tatia burdett <tatiaburdett@gmail.com>
2020-12-14 19:39:59 -0600

    function stubs

--- app.js
+++ app.js
@@ -30,29 +30,62 @@ const store = {
   score: 0
 };
 
-/**
- * 
- * Technical requirements:
- * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
- * See your course material and access support for more details.
- *
- * NO additional HTML elements should be added to the index.html file.
- *
- * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
- *
- * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
- * 
- */
 
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
 // These functions return HTML templates
 
+function generateStartHtml() {
+  console.log('generateStartHtml ran');
+}
+
+function generateQuestionScoreNumHtml() {
+  console.log('generateQuestionScoreNumHtml ran')
+}
+
+function generateQuestionsHtml() {
+  console.log('generateQuestionHtml ran');
+}
+
+function generateResultHtml() {
+  console.log('generateResultHtml ran ran');
+}
+
+function generateFinalScreenHtml() {
+  console.log('generateFinalScreenHtml ran');
+}
+
 /********** RENDER FUNCTION(S) **********/
 
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
 
+function renderQuiz() {
+  console.log('renderQuiz ran')
+}
+
 /********** EVENT HANDLER FUNCTIONS **********/
 
 // These functions handle events (submit, click, etc)
+
+function handleStartQuizClick() {
+  console.log('handleStartQuizClick ran');
+}
+
+function handleSubmitQuestionClick() {
+  console.log('handleSubmitQuestionClick ran')
+}
+
+function handleNextQuestionClick() {
+  console.log('handleNextQuestionClick ran')
+}
+
+/********** Quiz **********/
+
+function handleQuizApp() {
+  renderQuiz();
+  handleStartQuizClick();
+  handleSubmitQuestionClick();
+  handleNextQuestionClick();
+}
+
+$(handleQuizApp)
